844f37c26aec3a38302d640f383b68da14a31e8c
Andrii Maliuta <quadr988@gmail.com>
2020-05-23 12:01:22 +0300

    removed http , added servet request test

--- src/main/java/com/anma/bh/sb/springtesting/controllers/HomeController.java
+++ src/main/java/com/anma/bh/sb/springtesting/controllers/HomeController.java
@@ -3,12 +3,53 @@ package com.anma.bh.sb.springtesting.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.Map;
+
 @Controller
 public class HomeController {
 
     @RequestMapping({"/home", "/"})
-    public String hetHomePage() {
+    public String hetHomePage(HttpServletRequest request) {
+
+        System.out.println("Context Path = " + request.getContextPath());
+        ;
+        System.out.println("Remote user = " + request.getRemoteUser());
+        ;
+        System.out.println("Servlet Path == " + request.getServletPath());
+        ;
+        System.out.println("Path info == " + request.getPathInfo());
+        System.out.println("URI == " + request.getRequestURI());
+        System.out.println("URL == " + request.getRequestURL());
+        System.out.println("Session last access time == " + request.getSession());
+
+        System.out.println("+++++++++ Headers:");
+        Enumeration<String> headerStrings = request.getHeaderNames();
+
+//        while (headerStrings.hasMoreElements()) {
+//            System.out.println(headerStrings.nextElement() + " ::: " + request.getHeader(headerStrings.nextElement()));
+//        }
+
+        Enumeration<String> attrNames = request.getAttributeNames();
+
+        System.out.println("++++++++ Attributes:");
+        while (attrNames.hasMoreElements()) {
+            System.out.println(attrNames.nextElement());
+        }
+
+        Map<String, String[]> paramsMap = request.getParameterMap();
+
+        Enumeration<String> paramNames = request.getParameterNames();
+
+        System.out.println("++++++++ Parameters:");
+
+        while (paramNames.hasMoreElements()) {
+            System.out.println(paramNames.nextElement());
+        }
+//        request.getParameterValues();
 
         return "home";
+
     }
 }
