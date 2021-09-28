5c264b33e15ecca6c5aec827980a8f8170b28499
andrii <quadr988@gmail.com>
2021-09-28 13:52:23 +0300

    excel

--- src/main/java/com/anma/bh/sb/springtesting/core/controllers/ExcelController.java
+++ src/main/java/com/anma/bh/sb/springtesting/core/controllers/ExcelController.java
@@ -1,5 +1,6 @@
 package com.anma.bh.sb.springtesting.core.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -12,6 +13,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Controller
+@Slf4j
 public class ExcelController {
 
     @GetMapping("/excel")
@@ -28,6 +30,7 @@ public class ExcelController {
         cell3.setCellValue("Age");
         Cell cell4 = firstRow.createCell(3);
         cell4.setCellValue("Color");
+
         for (int i = 1; i <= 3; i++) {
             Row row = sheet.createRow(i);
             for (int j = 0; j < 4; j++) {
@@ -35,7 +38,12 @@ public class ExcelController {
                 cell.setCellValue("TEST");
             }
         }
-        final String FILE_NAME = "test-excel.xlsx";
+        log.info(ExcelController.class.getClassLoader().getResource("static/files/doc-test-1.docx").toString());
+        //  /home/andrii/programming/spring/spring-boot-features-test/target/spring-testing-0.0.2.jar!/BOOT-INF/classes!/files/test
+
+//        final String FILE_NAME = "/home/andrii/programming/spring/spring-boot-features-test/src/main/resources/files/test-excel.xlsx";
+//        final String FILE_NAME = "/home/andrii/programming/spring/spring-boot-features-test/target/spring-testing-0.0.2.jar!/BOOT-INF/classes!/static/files/test-excel.xlsx";
+        final String FILE_NAME = "/test-excel.xlsx";
         try {
             FileOutputStream stream = new FileOutputStream(FILE_NAME);
             workbook.write(stream);
